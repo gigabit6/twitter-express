9fbcce8442652fe88b2bbf7de990084eb40a7466
Gergana Dobrikova <g.dobrikova@gmail.com>
2019-03-04 20:24:29 +0200

    Add Support for local development

--- server/config/settings.js
+++ server/config/settings.js
@@ -5,8 +5,8 @@ let rootPath = path.normalize(path.join(__dirname, '/../../'))
 module.exports = {
   development: {
     rootPath: rootPath,
-    db: process.env.MONGODB_URI,
-    port: process.env.PORT
+    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/twitter2',
+    port: process.env.PORT || 3030
   },
   staging: {
   },
