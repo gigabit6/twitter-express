46ac2abcee1ca52545f0e436977a26b373e716d3
Gergana Dobrikova <g.dobrikova@gmail.com>
2019-03-04 16:05:49 +0200

    Add Heroku Support

--- server/config/settings.js
+++ server/config/settings.js
@@ -5,7 +5,7 @@ let rootPath = path.normalize(path.join(__dirname, '/../../'))
 module.exports = {
   development: {
     rootPath: rootPath,
-    db: 'mongodb://localhost:27017/twitter2',
+    db: process.env.MONGODB_URI,
     port: 3030
   },
   staging: {
